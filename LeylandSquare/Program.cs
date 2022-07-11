1c25bcaee4ba3c8ff85526cb4d2706a30afa1f8a
cianj <cianj@LAPTOP-TV25HSF3>
2022-07-11 16:24:08 +0100

    Add project files.

---
+++ LeylandSquare/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LeylandSquare
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            bool square;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                square = isSquare(i);
+                Console.WriteLine("Is " + i + " a square number? " + square);
+            }
+        }
+
+
+        public static bool isSquare(int input)
+        {
+            if (input < 0)
+            {
+                return false;
+            }
+            else if (Math.Sqrt(input) % 1 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
